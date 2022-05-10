e04267b5a8af2edf6f37eeaca9efec2d94f18127
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2022-05-09 21:40:38 -0400

    Added Folders and Files
    Added packages to organize files
    Implemented singleton class for dbconnection

---
+++ src/DbHelper/DbConnector.java
@@ -0,0 +1,33 @@
+package DbHelper;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnector {
+    
+    // Singleton class
+    public static Connection createConnection(){
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:src/DataBase/Bankdb.db");
+            return con;
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("SQL DRIVER IS NOT FOUND [" +e+"]");
+            System.exit(0);
+        }
+        catch (SQLException ex){
+            System.out.println("SQL EXCEPTION [" +ex+"]");
+        }
+
+        System.out.println("SUCCESSFULLY CONNECTION");
+        return null;
+    }
+
+}
